fef66928d39897c94b699af3c1dd3c49183a853a
Maheshwar098 <107045447+Maheshwar098@users.noreply.github.com>
2023-05-10 20:11:00 +0530

    fixed portpolio page orientation

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -21,45 +21,43 @@ const Portfolio = () => {
 
   return (
     <div className="container p-0 p-sm-5">
-      <div className="h3 ipoupcoming mb-3 mb-sm-4 text-center">Portfolio</div>
+      <div className="h3 ipoupcoming mb-3 mb-sm-4 text-center">Portfolio
+      </div>
 
       <br />
 
-      <div className="row row-cols-1 g-4 g-sm-3">
-        <div className="container">
+      <div>
+        <div className="container mb-4">
           <div
-            className="card mx-sm-auto mx-3 shadow"
-            style={{
-              backgroundColor: "#3d3d4c",
-              color: "#fefdff",
-              borderRadius: "10px",
-            }}
+            className="card shadow"
+            style={{ backgroundColor: "#3d3d4c", color: "#fefdff", borderRadius: "10px" }}
           >
             <div className="card-body" style={{ padding: "12px" }}>
-              <div className="d-flex justify-content-around">
-                <div className="fw-bold">Company</div>
-                <div className="fw-bold">No of Lots</div>
-                <div className="fw-bold">Amt</div>
+              <div className="d-flex justify-content-around font-weight-bold">
+              <p class="mb-0">Companay Name</p>
+              <p class="mb-0">Quantity</p>
+              <p class="mb-0">Offer Bid</p>
               </div>
             </div>
           </div>
         </div>
-        
-        {!portfolioCard && (
+      </div>
+
+      {!portfolioCard && (
         <ThreeDots
-        height="55"
-        width="55"
-        color="#5eb5f8"
-        ariaLabel="line-wave"
-        wrapperClass="loader"
-        visible={true}
-        firstLineColor=""
-        middleLineColor=""
-        lastLineColor=""
-      />
+          height="55"
+          width="55"
+          color="#5eb5f8"
+          ariaLabel="line-wave"
+          wrapperClass="loader"
+          visible={true}
+          firstLineColor=""
+          middleLineColor=""
+          lastLineColor=""
+        />
       )}
 
-      <div className="row row-cols-1 g-4 g-sm-3 mx-sm-auto mx-0">
+      <div className="row row-cols-1 gy-3 g-sm-3 mx-sm-auto">
         {portfolioCard &&
           (<>
             {portfolioCard.map((card) => {
@@ -69,8 +67,8 @@ const Portfolio = () => {
         }
       </div>
 
-      </div>
     </div>
+   
   );
 };
 
